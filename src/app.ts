5ee97d11073542af25b85e66d471ab47ac520a85
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-10-01 15:14:48 -0500

    Add DB Config file and test

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express  from 'express';
+import pool from './database/db_connect';
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/',(req, res) => {
+app.get('/',async (req, res) => {
+    const query ='select * from employees;';
+    const response = await pool.query(query);
+    console.log(response);
     res.send('hola mundo jejejeje ');
 });
 app.listen(port, () =>{
