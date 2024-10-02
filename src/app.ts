e3c62576751b162510b2ecdcb0bdcd4561166bdb
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-10-02 11:45:40 -0500

    Add Controller for categories

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
-import express  from 'express';
+import express, { Router }  from 'express';
 import pool from './database/db_connect';
+import { getCategories } from "./controllers/categories_controller";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/',async (req, res) => {
-    const query ='select * from employees;';
-    const response = await pool.query(query);
-    console.log(response);
+const categoriesRoutes = Router();
+
+categoriesRoutes.get('/categories',getCategories);
+
+/*app.get('/',async (req, res) => {
+    //const query ='select * from employees;';
+    //const response = await pool.query(query);
+    //console.log(response);
     res.send('hola mundo jejejeje ');
-});
+});*/
+
+app.use(categoriesRoutes);
+
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
 });
