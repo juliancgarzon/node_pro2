25dac7648e11d985e95b5c1cf7785d87d984b3b4
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-10-09 11:42:13 -0500

    states routes

--- src/routes/states_routes.ts
+++ src/routes/states_routes.ts
@@ -4,7 +4,7 @@ import { createState, deleteStates, getus_states, getus_statesById, updateStates
 
 export const us_statesRoutes =Router();
 
-us_statesRoutes.get('/us_states',authenticateToken,getus_states)
+us_statesRoutes.get('/us_states',getus_states)
 us_statesRoutes.get('/us_states/:id',authenticateToken,getus_statesById);
 us_statesRoutes.post('/createState',authenticateToken,createState );
 us_statesRoutes.delete('/deleteState/:id',authenticateToken,deleteStates);
