88e3609b294caaac4950e538b460697bad2693e9
julian camilo garzon lopez <julianmac@MacBook-Pro-de-julian.local>
2024-11-14 12:59:25 -0500

    modify user_controller

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,20 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import pool from "../database/db_connect";
-
+import bcrypt from "bcrypt";
 
 require("dotenv").config();
 
-export const generateToken = async(req: Request, response: Response): Promise <Response> => {
+export const generateToken = async(req: Request, response: Response): Promise<Response> => {
     const usuario = req.body.username;
     const contraseña = req.body.password;
-    const query = await pool.query('SELECT * FROM user_login WHERE usuario = $1 AND contraseña = $2', [usuario, contraseña]);
+    const query = await pool.query('SELECT * FROM user_login WHERE usuario = $1', [usuario]);
     const user = query.rows[0];
-    if (query.rowCount !== null  && query.rowCount > 0){
-    const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}`, {expiresIn: "1h",});
-    return response.status(200).json({ accessToken });
+
+    if (user && await bcrypt.compare(contraseña, user.contraseña)) {
+        const accessToken = jwt.sign({ id: user.id, usuario: user.usuario }, `${process.env.CLAVE_JWT}`, { expiresIn: "1h" });
+        return response.status(200).json({ accessToken });
     } else {
-    return response.status(400).json('User Not found');
+        return response.status(400).json('User Not found');
     }
 };
 
